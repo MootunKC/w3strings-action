9d3fa4ab1590b62477f791cec20709f3e872d93f
Abheek Dhawan <abheekd@protonmail.com>
2022-03-03 14:57:02 -0600

    Attempt to add support for language and output directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,43 @@
 #!/bin/sh -l
 
-/w3strings/w3strings -e "$1" -i "$2" --very-verbose
+CSV_FILE="$1"
+ID_SPACE="$2"
+OUTPUT_DIR="$3"
+OUTPUT_LANG="$4"
+
+ALL_LANGUAGES=("ar" "br" "cn" "cz" "de" "en" "es" "esmx" "fr" "hu" "it" "jp" "kr" "pl" "ru" "zh")
+
+mkdir -p "$OUTPUT_DIR"
+
+check_language_validity "$OUTPUT_LANG"
+
+if [[ "$OUTPUTFILE" = "all" ]]
+then
+    for lang in $ALL_LANGUAGES
+    do
+        encode "$lang.w3strings"
+    done
+else
+    encode "$OUTPUT_LANG.w3strings"
+fi
+
+function encode {
+    OUTPUT="$1"
+    /w3strings/w3strings -e "$CSV_FILE" "$OUTPUT" -i "$ID_SPACE" --very-verbose
+}
+
+function check_language_validity {
+    LANGUAGE="$1"
+    VALID_LANGUAGES="$ALL_LANGUAGES"
+    for lang in VALID_LANGUAGES
+    do
+        if [[ "$LANGUAGE" = lang ]]
+        then
+            break;
+        fi
+    done
+
+    # If the language isn't found...
+    echo "Invalid language!"
+    exit 1
+}
